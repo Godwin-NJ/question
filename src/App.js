d0a2699db857ee2c038ef36a00c51a389bf0f284
Bash Ola <bashmbash@yahoo.com>
2020-12-22 07:29:06 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+// import questions from './data';
+import data from './data';
+import SingleQuestion from './Question';
+
+
+function App() {
+  const[questions, setQuestion] = useState(data);
+
+  return (
+    <main>
+      <div className='container'>
+        <h3>questions and answers about login</h3>
+        <section className='info'>
+          {questions.map((question) => {
+           return < SingleQuestion {...question}  key={question.id}/>
+          })}
+        </section>
+      </div>
+    </main>
+  )
+}
+
+
+
+
+export default App;
